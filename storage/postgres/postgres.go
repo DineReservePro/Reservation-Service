c12336aa1aa13e0298ba35c703322bf7fe74bb85
Jons <ozodjon160605@gmail.com>
2024-07-08 20:29:15 +0500

    ok

--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -4,20 +4,21 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "BEKJONS"
-	dbname   = "res_m"
+	"reservation-service/config"
 )
 
 func Conn() (*sql.DB, error) {
+	cfg := config.Config{}
 	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		cfg.DB_HOST, cfg.DB_PORT, cfg.DB_USERNAME, cfg.DB_PASSWORD, cfg.DB_DATABASE)
 
 	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
 	return db, err
 }
